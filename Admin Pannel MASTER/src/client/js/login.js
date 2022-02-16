f315f45e2445459d57081f897c9ff814bbc7bf65
Grant Baumstark <gbaumstark@micds.org>
2022-02-16 12:39:06 -0600

    admin login

--- Admin Pannel MASTER/src/client/js/login.js
+++ Admin Pannel MASTER/src/client/js/login.js
@@ -1,1 +1,17 @@
 console.log('Test')
+function AdminLogin() {
+    const user_base_url = "mymicds.net/admin";
+    class CustomerService{
+        authenticateUser(user) {
+            return axios.post(user_base_url+ '/login', user)
+             .then((res)=>{
+               if(res.data === 'SUCCESS') {
+                 //user logged in, Success
+               } else if(res.data === 'FAILURE') {
+                 //login failed, try again later
+               }})
+             .catch(err) {
+               console.error(err)
+             }
+        }
+    }
