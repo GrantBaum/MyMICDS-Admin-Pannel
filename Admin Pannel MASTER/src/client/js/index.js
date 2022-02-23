e5f0ae8fc5d3b39c7cdd20668157fe557f1642fe
Grant Baumstark <gbaumstark@micds.org>
2022-02-22 18:01:19 -0600

    Shutdown
    still need to add restart button, but i will probs make it so that the Restart displays only when the website is shutdown.

--- Admin Pannel MASTER/src/client/js/index.js
+++ Admin Pannel MASTER/src/client/js/index.js
@@ -2,3 +2,10 @@ console.log('Test')
 function TakeOverTheWorld() {
     location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
 }
+function ShutdownAlert() {
+    alert("This will shut down MyMICDS untill it is manually reactivated again. Are you sure you want to continue?")
+};
+    function RestartAlert() {
+        alert("This will reactivate manually reactivate MyMICDS. Are you sure you want to continue?")
+    };
+   
