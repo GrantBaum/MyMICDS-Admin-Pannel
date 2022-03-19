710b97e576674d3d330b515411f4feea7b6c5505
Grant Baumstark <gbaumstark@micds.org>
2022-03-19 17:39:38 -0500

    update number i cant even fricking keep track anymore

--- Admin Pannel MASTER/src/client/js/index.js
+++ Admin Pannel MASTER/src/client/js/index.js
@@ -2,9 +2,19 @@ console.log('Test')
 function TakeOverTheWorld() {
     location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
 }
-function ShutdownAlert() {
+function ShutdownAction() {
     alert("This will shut down MyMICDS untill it is manually reactivated again. Are you sure you want to continue?")
+    location.href = 'restart.html'
+    //@todo: make this function talk with api.index.ts which can in turn shut dowm the website
 };
-    function RestartAlert() {
+    function RestartAction() {
         alert("This will reactivate manually reactivate MyMICDS. Are you sure you want to continue?")
+        location.href = 'shutdown.html'
+        //@todo: make this function talk with api.index.ts which can in turn restart the website
     };
+    // const socket = io('https://io.mymicds.net');
+			const socket = io('http://localhost:1421');
+
+			const $label = $('.label');
+			const $debug = $('.debug');
+			const $password = $('.password');
