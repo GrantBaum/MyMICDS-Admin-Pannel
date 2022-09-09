982515dd06687c4f440b3cc832cbd58c879e643f
Grant Baumstark <gbaumstark@micds.org>
2022-09-09 11:40:03 -0500

    fixed js by moving it to html file

--- Admin Pannel MASTER/src/client/js/announce.js
+++
@@ -1,9 +0,0 @@
-console.log('TEST')
-function Publish() {
-alert("Are you sure you want to publish this?")
-}
-
-
-function Clear() {
-	 alert("Are you sure you want to clear the current active announcement?")
-}
