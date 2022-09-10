05632ccf77aff8acc8c573c94a7fba27410a3854
Grant Baumstark <gbaumstark@micds.org>
2022-09-10 11:03:36 -0500

    simple login variable grabber

--- Admin Pannel MASTER/src/client/js/login.js
+++ Admin Pannel MASTER/src/client/js/login.js
@@ -1,2 +1,8 @@
 console.log('Test')
 mymicds.auth.login()
+var user = document.getElementsByName("user")[0].value;
+var pass = document.getElementsByName("pass")[0].value;
+function formChanged() {
+    var user = ''
+    var pass = ''
+}
