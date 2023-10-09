2470a155fabbb157456d220f7a756986e06d78d1
Grant Baumstark <gbaumstark@micds.org>
2023-10-09 11:31:52 -0500

    Update offlinelogic.js

--- Admin Pannel MASTER/src/client/js/offlinelogic.js
+++ Admin Pannel MASTER/src/client/js/offlinelogic.js
@@ -1,5 +1,17 @@
 var web = true
 
-function checkWeb(){
-    //check if the computer that is running the site is connected to the internet
-}
+ function checkInternetConnection() {
+            fetch('https://www.google.com', { mode: 'no-cors' })
+                .then(() => {
+                    // Internet connection is available, hide the "no internet" content
+                    document.getElementById('no-internet').style.display = 'none';
+                })
+                .catch(() => {
+                    // No internet connection, display the "no internet" content
+                    document.getElementById('content').style.display = 'none';
+                    document.getElementById('no-internet').style.display = 'block';
+                });
+        }
+
+        // Check internet connectivity when the page loads
+        window.addEventListener('load', checkInternetConnection);
